bbfbdd8520d2d53bed0752a2536cfb9593f8e0ce
= <anchallsm2000@gmail.com>
2023-05-29 23:24:20 +0530

    first commit

---
+++ client/src/App.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import { Route, Routes } from 'react-router-dom';
+
+const App = () => {
+  return (
+    <div className='relative sm:-8 p-4 bg-[#13131a] min-h-screen flex flex-row'>
+      <div className='sm:flex hidden mr-10 relative bg-[#cec7c7]'>
+        sidebar
+      </div>
+      <div className='flex-1 max-sm:w-full max-w-[1280px] mx-auto sm:pr-5 bg-[#cec7c7]'>
+        navbar
+      </div>
+    </div>
+  )
+}
+
+export default App
--- web3/contracts/Crowdfunding.sol
+++ web3/contracts/Crowdfunding.sol
@@ -10,8 +10,7 @@ contract Crowdfunding
         uint256 target;
         uint256 amountCollected;
         uint256 deadline;
-        string imgage; 
-        uint256 amount;
+        string image; 
         address[] donators;
         uint256[] donations;
     }
@@ -20,7 +19,8 @@ contract Crowdfunding
 
     uint256 public numberOfCompaign=0;
 
-    function createCompaign(address _owner, string memory _title, uint256 _deadline) public returns (uint256)
+    function createCompaign(address _owner, string memory _title, string memory _description, 
+            uint256 _target, string memory _image, uint256 _deadline) public returns (uint256)
     {
 
         //before creating compaign check if everything is Ok!!
@@ -29,8 +29,44 @@ contract Crowdfunding
         Compaign storage compaign = compaigns[numberOfCompaign];
 
         compaign.owner = _owner;
+        compaign.title = _title;
+        compaign.description = _description;
+        compaign.target = _target;
+        compaign.image = _image;
+        compaign.deadline = _deadline;
 
         numberOfCompaign++;
         return numberOfCompaign-1;
+    }
+
+    function donateToCompaign(uint256 _id) public payable {
+        uint256 amount = msg.value;
+        Compaign storage campaign = compaigns[_id];
+        campaign.donators.push(msg.sender);
+        campaign.donations.push(amount);
+
+        (bool sent,) = payable(campaign.owner).call{value:amount}("");
+
+        if(sent)
+        {
+            campaign.amountCollected = campaign.amountCollected+amount;
+        }
+        
+    }
+
+    function getDonators(uint256 _id) view public returns (address[] memory,uint256[] memory)
+    {
+        return (compaigns[_id].donators,compaigns[_id].donations);
+    }
+
+    function getCampaigns() public view returns (Compaign[] memory) {
+        Compaign[] memory allCampaigns = new Compaign[](numberOfCompaign);
+
+        for(uint i=0;i<numberOfCompaign;i++)
+        {
+            Compaign storage item = compaigns[i];
+            allCampaigns[i] = item;
+        }
+        return allCampaigns;
     }
 }
